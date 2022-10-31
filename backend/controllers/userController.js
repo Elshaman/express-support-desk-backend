70fa2d026ce41d081a4acb9d1964b6d241cbe2d3
Cristian <cbo@misena.edu.co>
2022-10-31 12:16:44 -0500

    login

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -42,5 +42,17 @@ exports.register = asyncHandler(
 )   
 
 exports.login=asyncHandler(async(req, res)=>{
-    res.send("login")
+    const {email, password} = req.body
+    const user = await User.findOne({email})
+    if(user && (await bcrypt.compare(password, user.password))){
+            res.status(200).json({
+                _id:user._id,
+                name:user.name,
+                email:user.email
+            })
+    }else{
+        res.status(401)
+        throw new Error('Invalid credentials')
+    }
+
 })
