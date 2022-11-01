b6be7b6aee82caf37bd52f367d18cae0ff0015d1
Cristian <cbo@misena.edu.co>
2022-11-01 05:07:34 -0500

    primeros componentes react

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,26 @@
 import React from 'react';
+import Header from './components/Header'
+import {BrowserRouter as Router, Routes,Route} from 'react-router-dom'
+import Home from './pages/Home'
+import Login from './pages/Login';
+import Register from './pages/Register';
+
 
 
 function App() {
   return (
-    <div>
-     My app
-    </div>
+    <>
+     <Router>
+        <div className='container'>
+          <Header />
+          <Routes>
+            <Route path='/' element={ <Home /> } />
+            <Route path='/login' element={ <Login /> } />
+            <Route path='/register' element={ <Register /> } />
+          </Routes>
+        </div>
+     </Router>
+    </>
   );
 }
 
