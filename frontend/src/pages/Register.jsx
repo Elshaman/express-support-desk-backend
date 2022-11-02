be6867f778d7d825c19e1fa3bb6a0399537f03a9
Cristian Buitrago <cbo@misena.edu.co>
2022-11-02 05:54:29 -0500

    redix 2

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,6 +1,8 @@
 import {useState} from 'react'
 import {toast} from 'react-toastify'
 import {FaUser} from 'react-icons/fa'
+import {useSelector, useDispatch}  from 'react-redux'
+import {register} from  '../features/auth/authSlice'
 
 function Register(){
 
@@ -13,6 +15,9 @@ function Register(){
 
     const {name, email, password, password2 } = formData
 
+    const dispatch = useDispatch()
+    const {user, isLoading, isSuccess, message} = useSelector(state => state.auth) 
+
     const onChange = (e) =>{
         setFormData((prevState) =>({
             ...prevState,
@@ -23,8 +28,15 @@ function Register(){
 
     const onSubmit = e =>{
         e.preventDefault()
-        if(!password !== password2){
+        if(password !== password2){
             toast.error('passwords do not match')
+        }else{
+            const userData ={
+                name,
+                email,
+                password
+            } 
+            dispatch(register(userData))
         }
     }
 
@@ -33,7 +45,7 @@ function Register(){
         <>
             <section className="heading">
                 <h1>
-                    <FaUser /> Register
+                    <FaUser /> Register {user} 
                 </h1>
                 <p> Please create an account</p>
             </section>
